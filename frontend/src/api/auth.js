afdc54f00d5de3f9b8e8506c2946ad45983e11e6
Mina Dong <minadong404@gmail.com>
2025-07-31 13:35:08 +0900

    feat: fe-로그인

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//백엔드 API주소 - 변경 필요 - axios작동 확인을 위해 json-server 전역설치 후 포트 4000 열어서 테스트함.
+// 백엔드 API주소 - 변경 필요
 const BASE_URL = 'http://localhost:4000';
 
 export const signup = async (data)=>{
@@ -8,8 +8,15 @@ export const signup = async (data)=>{
     return response.data
 }
 
+export const login = async (data)=>{
+    const response = await axios.post(`${BASE_URL}/api/login`, data)
+    return response.data
+}
+
 
 //////////////////////// 더미데이터 확인용
+
+////// 회원가입 더미데이터
 // export const signup = async (data) => {
 //   console.log('더미 회원가입 데이터:', data);
 
@@ -21,4 +28,20 @@ export const signup = async (data)=>{
 //       });
 //     }, 1000);
 //   });
+// };
+
+////// 로그인 더미데이터
+// export const login = async (data) => {
+//   console.log('Mock login 요청됨:', data);
+
+//   if (data.email !== 'admin@aivle.co.kr' || data.password !== '1234') {
+//     throw new Error('잘못된 로그인 정보');
+//   }
+
+//   return {
+//     accessToken: 'mockAccessToken123',
+//     refreshToken: 'mockRefreshToken456',
+//     role: 'ROLE_USER',
+//     message: '로그인 성공!',
+//   };
 // };
