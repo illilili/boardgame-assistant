e6ef77f0996f8daedb4a3ef9dcea178ee96cd35f
junghoon <dksldsk@naver.com>
2025-08-04 10:33:24 +0900

    feat: 기획서 오류 수정

--- backend-spring/src/main/java/com/boardgame/backend_spring/project/repository/ProjectMemberRepository.java
+++ backend-spring/src/main/java/com/boardgame/backend_spring/project/repository/ProjectMemberRepository.java
@@ -1,4 +1,7 @@
 package com.boardgame.backend_spring.project.repository;
 
-public class ProjectMemberRepository {
+import com.boardgame.backend_spring.project.entity.ProjectMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
 }
